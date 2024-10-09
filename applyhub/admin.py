6ae9186b9649df19106cdad7b6755d4be7b5944c
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-09 15:55:52 +0000

    feat: register application model to admin site

--- applyhub/admin.py
+++ applyhub/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+
+# Register application model to admin site
+@admin.register(models.Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Application model.
+    
+    - Displays key fields such as position, company name, position level, date applied, status, and the applicant.
+    - Allows searching by position to quickly find applications.
+    - Filters the applications by status and creation date for better management in the admin interface.
+    - Automatically generates the slug field based on the position field for SEO-friendly URLs.
+    """
+    list_display = ["position", "company_name", "position_level", "date_applied", "status", "applicant"]
+    search_fields = ['position']
+    list_filter = ('status','created_on',)
+    prepopulated_fields = {'slug': ('position',)}
