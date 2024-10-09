891f2f5e94f348b224c3913b7d81e6aee1046f0a
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-09 16:23:45 +0000

    feat: add profileimage model

--- applywise/settings.py
+++ applywise/settings.py
@@ -30,7 +30,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.8000-cimustafa-applywise-8y40e6k5n32.ws.codeinstitute-ide.net",
 ]
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-cimustafa-applywise-8y40e6k5n32.ws.codeinstitute-ide.net'
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'applyhub',
 ]
 
