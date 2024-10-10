52f4927a6f31d8aa5b859d739d04623595e92234
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-10 18:20:13 +0000

    feat: list jobs on the page, count the jobs based on status of the jobs

--- applyhub/views.py
+++ applyhub/views.py
@@ -11,6 +11,16 @@ def application_list(request):
     # Check if the user is authenticated
     if request.user.is_authenticated:
         applications = models.Application.objects.filter(applicant=request.user)
+        application_count = applications.count()
+        applied_apps_count = applications.filter(status="applied".capitalize()).count()
+        interview_apps_count = applications.filter(status="interview".capitalize()).count()
+        offered_apps_count = applications.filter(status="offer".capitalize()).count()
+        rejected_apps_count = applications.filter(status="rejected".capitalize()).count()
     return render(request, 'index.html', {"applications": applications,
-                                          "no_application_message": no_application_message})
+                                          "no_application_message": no_application_message,
+                                          "application_count": application_count,
+                                          "interview_apps_count": interview_apps_count,
+                                          "offered_apps_count": offered_apps_count,
+                                          "rejected_apps_count": rejected_apps_count,
+                                          "applied_apps_count": applied_apps_count})
 
