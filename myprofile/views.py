ddad0fce317eb8a8260b6e9629919681f5637c66
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-17 17:20:40 +0000

    feat: display a default profile image for every user

--- myprofile/views.py
+++ myprofile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from . import forms
+from . import models
 
 def profile(request, pk):
     myprofile = get_object_or_404(User, pk=pk)
