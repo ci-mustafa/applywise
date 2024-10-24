2dec7ee8434226ea7eb52bbc60fb3f08d1627dc4
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-24 09:47:49 +0000

    feat: save contact form data to database

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from . import forms
 from . import models
 
 def contact(request):
-    contact_form = forms.ContactForm()
+    if request.method == "POST":
+        contact_form = forms.ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, "Your message has been sent successfully!")
+            return redirect('contact')  
+    else:
+        contact_form = forms.ContactForm()
     return render(request, 'contact.html', {
         'contact_form': contact_form
     })
