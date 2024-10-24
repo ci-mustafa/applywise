c73d7d8e80c1e4151cc42ac886efd5e7ce912bdf
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-24 08:55:16 +0000

    feat: add model for contact app, register it to admin site

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+# contact model admin
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'message']
