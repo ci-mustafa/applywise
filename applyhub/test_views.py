79cb811566c46831546f84ffa1c273d30f777ae6
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-29 15:05:20 +0000

    feat: add testcase for post request of application creation

--- applyhub/test_views.py
+++ applyhub/test_views.py
@@ -68,3 +68,55 @@ class ApplicationListViewTests(TestCase):
         
         # Check that the response is a redirect to the login page
         self.assertRedirects(response, '/accounts/login/?next=/')
+
+
+class CreateApplicationViewTests(TestCase):
+
+    def setUp(self):
+        """Create a test user and set up initial data for the tests."""
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.client.login(username='testuser', password='password123')  # Log the user in
+
+    def test_create_application_post(self):
+        """Test that a valid application can be created through a POST request."""
+        data = {
+            'position': 'Software Engineer',
+            'description': 'This is a test application',
+            'slug': 'software-engineer',  
+            'position_level': 'Junior',
+            'company_name': 'Test Company',
+            'date_applied': '2024-01-01',
+            'status': 'Applied',
+            'applied_method': 'Website',
+            'location': 'Remote',
+            'job_link': 'http://example.com/job',
+            'recruiter_contact': 'recruiter@example.com',
+            'follow_up_date': '2024-02-01'
+        }
+        
+        response = self.client.post(reverse('create-application'), data)
+        
+        # Check that a new application was created
+        self.assertEqual(Application.objects.count(), 1)
+        application = Application.objects.first()
+        
+        # Verify that all fields were saved correctly
+        self.assertEqual(application.position, 'Software Engineer')
+        self.assertEqual(application.description, 'This is a test application')
+        self.assertEqual(application.slug, 'software-engineer')
+        self.assertEqual(application.position_level, 'Junior')
+        self.assertEqual(application.company_name, 'Test Company')
+        self.assertEqual(application.date_applied.strftime('%Y-%m-%d'), '2024-01-01')
+        self.assertEqual(application.status, 'Applied')
+        self.assertEqual(application.applied_method, 'Website')
+        self.assertEqual(application.location, 'Remote')
+        self.assertEqual(application.job_link, 'http://example.com/job')
+        self.assertEqual(application.recruiter_contact, 'recruiter@example.com')
+        self.assertEqual(application.follow_up_date.strftime('%Y-%m-%d'), '2024-02-01')
+
+        # Check that the application is associated with the logged-in user
+        self.assertEqual(application.applicant, self.user)
+        
+        # Check that the response is a redirect to the home page
+        self.assertRedirects(response, reverse('home'))
+
