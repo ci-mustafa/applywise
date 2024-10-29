13445ffd6f1a7a4300469c6fe828fb12ccb4a009
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-10-29 15:40:34 +0000

    feat: add testcase for about app

---
+++ about/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import About
+
+class AboutViewTests(TestCase):
+
+    def setUp(self):
+        """Create a test About object to be used in the tests."""
+        self.about_instance = About.objects.create(
+            developer_image='path/to/image.jpg', 
+            title='About Us',
+            content='This is the about page content.'
+        )
+
+    def test_about_view(self):
+        """Test that the about view returns the correct template and context."""
+        response = self.client.get(reverse('about')) 
+
+        # Check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct template is used
+        self.assertTemplateUsed(response, 'about.html')
+
+        # Check that the context contains the About instance
+        self.assertEqual(response.context['about_app'], self.about_instance)
+
+
