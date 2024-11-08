5a63fcbbf88179910a44fe193e147f2c1964396a
Mustafa Akbari <akbarimustafa64@gmail.com>
2024-11-08 21:05:55 +0000

    feat: add docstring for admin and views files of about app

--- about/admin.py
+++ about/admin.py
@@ -4,4 +4,17 @@ from . import models
 
 @admin.register(models.About)
 class AboutAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the About model.
+
+    This class defines how the About model will be displayed in
+    the Django admin interface.
+    It specifies which fields will be shown in the list display
+    and can be customized further
+    to allow filtering, searching, and other administrative functions.
+
+    Attributes:
+        list_display (list): A list of model fields to display
+        in the admin list view.
+    """
     list_display = ['developer_image', 'title', 'content', 'updated_on']
--- about/views.py
+++ about/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from . import models
 
 def about(request):
+    """
+    Render the 'About' page with application information.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered 'about.html' template
+        with context data.
+    """
     about_app = models.About.objects.all().first()
     return render(request, 'about.html', {
         'about_app': about_app
